8e08b544caefd6addcc1db6fa41efaf1e4c05a9d
Ken Ang <angkendrick@gmail.com>
2015-07-20 16:48:02 +0000

    completed add to db, success page and fail page with errors

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,29 @@
 get '/' do
   erb :index
 end
+
+get '/create' do
+  erb :create
+end
+
+get '/view' do
+  erb :view
+end
+
+get '/error' do
+  @track = Track.all
+  erb :error
+end
+
+post '/new' do
+  @track = Track.new(
+    song_title: params[:song_title],
+    author: params[:author],
+    url: params[:url]
+  )
+  if @track.save
+    erb :success 
+  else
+    erb :error
+  end
+end
